51d8751984d8a1244197f0ba634a97ad86295982
Sinval Vieira <sinvalneto01@gmail.com>
2016-09-22 09:16:18 -0300

    Updating setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,23 @@ with open('HISTORY.rst') as history_file:
 
 requirements = [
     'Click>=6.0',
+    'pip==8.1.2',
+    'bumpversion==0.5.3',
+    'wheel==0.29.0',
+    'watchdog==0.8.3',
+    'flake8==2.6.0',
+    'tox==2.3.1',
+    'coverage==4.1',
+    'Sphinx==1.4.4',
+    'click>=3.3,<7',
+    'keyring>=9,<10',
+    'parsedatetime>=1.4,<3',
+    'python-dateutil>=2.4.1,<3',
+    'requests>=2.4.3,<3.0',
+    'arrow>=0.5.4,<0.8',
+    'termcolor==1.1.0',
+    'six==1.9.0',
+    'pytest==2.9.2',
     # TODO: put package requirements here
 ]
 
@@ -20,13 +37,13 @@ test_requirements = [
 
 setup(
     name='glogcli',
-    version='0.2.0',
+    version='0.3.0',
     description="Graylog command line interface.",
     long_description=readme + '\n\n' + history,
     author="Sinval Vieira Mendes Neto",
     author_email='sinvalneto01@gmail.com',
     url='https://github.com/globocom/glog-cli',
-    download_url = 'https://github.com/globocom/glog-cli/tarball/0.2',
+    download_url = 'https://github.com/globocom/glog-cli/tarball/0.3',
     packages=[
         'glogcli',
     ],

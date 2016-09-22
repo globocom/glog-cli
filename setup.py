9a8a6623a52f3a4a5950fe9f95094cb89ba5354c
Sinval Vieira <sinvalneto01@gmail.com>
2016-09-22 17:38:52 -0300

    Tagging 0.4.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ test_requirements = [
 
 setup(
     name='glogcli',
-    version='0.3.0',
+    version='0.4.0',
     description="Graylog command line interface.",
     long_description=readme + '\n\n' + history,
     author="Sinval Vieira Mendes Neto",

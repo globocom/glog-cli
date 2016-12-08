df060fe6136010434b1c1d339e882bc051586ba7
Sinval Vieira <sinvalneto01@gmail.com>
2016-12-08 15:34:12 -0200

    Bumping to 0.7.0 version in setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ test_requirements = [
 
 setup(
     name='glogcli',
-    version='0.6.0',
+    version='0.7.0',
     description="Graylog command line interface.",
     long_description=readme + '\n\n' + history,
     author="Sinval Vieira Mendes Neto",
